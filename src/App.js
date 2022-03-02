9fa0b1d44e254d5983d61019c445b25bc89a0ebf
ramachandran27 <ramachandran052015@gmail.com>
2022-03-02 08:38:32 +0530

    Fetch API add the store-App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data,setData] = useState([])
+  const getData = async () => {
+    const data = await fetch("https://fakestoreapi.com/products");
+    const resp = await data.json();
+    setData(resp);
+  };
+  useEffect(()=>{
+    getData();
+  },[])
+  return <div className="App">Fetch API project</div>;
 }
 
 export default App;
